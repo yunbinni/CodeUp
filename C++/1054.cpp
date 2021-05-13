860535b1b0ca3b4dcb7b02f8df1efc3da9d31675
yunbinni <yunbin9049@gmail.com>
2021-05-13 11:49:26 +0900

    1053 ~ 1058 added

---
+++ C++/1054.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int a, b;
+    cin >> a >> b;
+    if(a != 0 && b != 0){
+        cout << 1;
+    } else{
+        cout << 0;
+    }
+}
---
+++ Java/1053.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        
+        int a = sc.nextInt();
+        
+        if(a == 0){
+            System.out.print(1);
+        } else{
+            System.out.print(0);
+        }
+    }
+}
